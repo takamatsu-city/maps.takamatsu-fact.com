4d1bf30fc1635f2abfd24dccc4c5be2c61c9bb1c
naoki <n.globe.us@gmail.com>
2023-03-10 14:23:57 +0900

    add header and sidebar

---
+++ src/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Content = () => {
+
+  return (
+    <>
+      <div className='sidebar'>
+      </div>
+    </>
+  );
+}
+
+export default Content;
