085c90131ae689f9746e145f765782c6b406afb8
naoki <n.globe.us@gmail.com>
2023-03-13 16:40:22 +0900

    add api link

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,8 +6,15 @@ const Content = () => {
   return (
     <>
       <header className="header">
-        <img className="logo" src='./logo.svg' alt="ロゴ"/>
-        <h2>高松スマートシティ地図 & API</h2>
+        <div className='brand'>
+          <img className="logo" src='./logo.svg' alt="ロゴ" />
+          <h2 className='title'>高松スマートシティ地図 & API</h2>
+        </div>
+        <div className='docs-link'>
+          <img src="./assets/file.svg" alt="ファイルアイコン" />
+          <p>APIドキュメンテーション</p>
+          <img src="./assets/arrow-right.svg" alt="矢印アイコン" />
+        </div>
       </header>
     </>
   );
