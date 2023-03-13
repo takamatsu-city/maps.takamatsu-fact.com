bc3bffef3b08483be85ee2a6b75608244c8c1e5d
naoki <n.globe.us@gmail.com>
2023-03-13 15:59:21 +0900

    fix header css

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.scss'
 
 const Content = () => {
 
   return (
     <>
       <header className="header">
+        <img className="logo" src='./logo.svg' alt="ロゴ"/>
+        <h2>高松スマートシティ地図 & API</h2>
       </header>
     </>
   );
