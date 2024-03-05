e9681f87e841300d0da28f4a6b5e78423e45d181
sugama-satsuki <piguchi@geolonia.com>
2024-03-05 13:39:36 +0900

    メンテナンス文を追加

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -160,6 +160,7 @@ const Sidebar: React.FC<SidebarProps> = ({ selectedLayers, setSelectedLayers, ca
       <a href="https://docs.takamatsu-fact.com/#%E3%81%94%E5%88%A9%E7%94%A8%E3%81%AB%E3%81%82%E3%81%9F%E3%81%A3%E3%81%A6" className='user-guide-link' target="_blank" rel="noreferrer">
         <AiOutlineLink /><span>ご利用にあたって</span>
       </a>
+      <div className='sidebar-news'>現在、センサーデータのうち「冠水状況」のデータが欠損しています。調査及び復旧作業を進めていますので、復旧までしばらくお待ちください。</div>
     </div>
   );
 }
