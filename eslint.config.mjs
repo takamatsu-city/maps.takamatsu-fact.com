3a46628f5fc47b8f5a81e55a3b1ffd56b0ad4824
sugama-satsuki <piguchi@geolonia.com>
2024-04-11 15:31:00 +0900

    ルールの記載

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,14 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'import/no-extraneous-dependencies': ['error', {
+        'devDependencies': ['**/*.stories.tsx', '**/*.test.tsx', '*.config.js']
+      }]
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
